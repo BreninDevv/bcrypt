c1278546ec8f507b723c8cdec707f05bb46223a6
Breno Henrique <brenoeevil@gmail.com>
2025-11-24 22:10:44 -0300

    entendendo como funciona, colocar hash no banco em andamento

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+async function connectDb() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Conectado ao banco");
+  } catch (error) {
+    console.log("error", error);
+  }
+}
 
 async function cadastroUsuario(senha) {
-  const hash = await bcrypt.hash(senha, 10);
-  console.log(senha, hash);
+  const hash = bcrypt.hash(senha, 10);
   return hash;
 }
 
-function loginUsuario() {}
-
-function fluxoUsuario() {
-  const hash = cadastroUsuario("1234");
-
-  const login = loginUsuario();
+async function loginUsuario(senha, hashSenha) {
+  const verify = await bcrypt.compare(senha, hashSenha);
+  return verify;
 }
+
+async function fluxoUsuario() {
+  const hash = await cadastroUsuario("1234");
+
+  const login = await loginUsuario("1234", hash);
+
+  console.log(login);
+}
+connectDb();
 fluxoUsuario();
